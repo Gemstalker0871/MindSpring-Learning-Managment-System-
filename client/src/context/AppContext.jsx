afb1ec3b2de6aeaab238d4f1a22aa88e40e73823
Sam <sambhavchugh2004@gmail.com>
2025-08-25 17:59:42 +0530

    Enhance CourseCard and CourseDetails components: add default educator name, implement course duration calculations, and improve layout with new Tailwind spacing configuration

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext()
 
@@ -33,11 +34,48 @@ export const AppContextProvider = (props) => {
         return totalRating / course.courseRatings.length
     }
 
+
+
+    //Calculate course chapter time
+
+    const calculateChapterTime = (chapter) => {
+        let time = 0
+        chapter.chapterContent.map((lecture) => time += lecture.lectureDuration)
+        return humanizeDuration(time * 60 * 1000, {units: ['h', 'm']})
+    }
+
+
+    //Calculate course duration
+
+    const calculateCourseDuration = (course) => {
+        let time = 0
+
+        course.courseContent.map((chapter)=> chapter.chapterContent.map(
+            (lecture) => time += lecture.lectureDuration
+        ))
+
+        return humanizeDuration(time * 60 * 1000, {units: ['h', 'm']})
+    }
+
+
+    //FUnction to calc no od lectures
+
+
+    const calculateNoOfLectures = (course) => {
+        let totalLectures = 0
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent.length
+            }
+        })
+        return totalLectures
+    }
+
     useEffect(()=>{
         fetchAllCourses()
     }, [])
     const value = {
-        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures
 
 
     }
