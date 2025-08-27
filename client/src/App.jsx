40c46cc14ae6df54c72d7c924efe142818d05faf
Sam <sambhavchugh2004@gmail.com>
2025-08-28 01:50:52 +0530

    Refactor educator components: update routing for Dashboard, implement data fetching and display for MyCourses and StudentsEnrolled, enhance layout with loading states and responsive design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
         {/* Educators */}
 
         <Route path='/educator' element = {<Educator />}>
-          <Route path='educator' element = {<Dashboard />}/>
+          <Route path='/educator' element = {<Dashboard />}/>
           <Route path='add-course' element = {<AddCourse />}/>
           <Route path='my-courses' element = {<MyCourses />}/>
           <Route path='students-enrolled' element = {<StudentsEnrolled />}/>
