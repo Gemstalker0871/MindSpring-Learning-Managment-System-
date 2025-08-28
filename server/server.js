ce93cc5a3e4198d4658214a1a46ddd80b5bcf9e1
Sam <sambhavchugh2004@gmail.com>
2025-08-29 01:25:22 +0530

    Add educator role update functionality and integrate Clerk middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerWebHooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educator.routes.js'
+import { clerkMiddleware } from '@clerk/express'
 
 //initialize express
 
@@ -13,10 +15,12 @@ await connectDB()
 
 //Middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
 //ROutes
 app.get('/', (req, res) => res.send("Api working"))
 app.post('/clerk', express.json(), clerWebHooks)
+app.use('/api/educator', express.json(), educatorRouter)
 
 //Port
 const PORT = process.env.PORT || 5000
@@ -25,3 +29,4 @@ app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
     
 })
+
